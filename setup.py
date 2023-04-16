1b16e82aa1425f50f90deda61f01d139568ad197
gsb13 <bhalekarganesh8446@gmail.com>
2023-04-16 16:19:42 +0530

    project info setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name = 'ML Project'
+versiom = '0.0.1'
+author = 'Ganesh bhaleakr'
+
+packages=find_packages()
+install_requires = ['pandas','numpy','seaborn']
+)
